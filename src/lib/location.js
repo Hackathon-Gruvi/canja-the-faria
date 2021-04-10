3c45303c55534bc142c04b5f0ee0e2e9fad4153a
duartefaria <up201607176@fe.up.pt>
2021-04-10 16:07:05 +0100

    fix location error

--- src/lib/location.js
+++ src/lib/location.js
@@ -2,13 +2,12 @@ const axios = require('axios');
 
 const getLocation = async () => {
   const locationData = await axios.get('http://ip-api.com/json');
-
   console.log(locationData);
 
   return {
-    status: locationData.status,
-    country: locationData.country,
-    city: locationData.city
+    status: locationData.data.status,
+    country: locationData.data.country,
+    city: locationData.data.city
   };
 }
 
